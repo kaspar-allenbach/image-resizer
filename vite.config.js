4e94ed17bd352e27bbbd6f04db7bb13fd6800e09
KSPR <robot@kasparallenbach.ch>
2025-09-02 16:38:42 +0200

    whatever dude

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/image-resizer/', // GitHub Pages repo name
+  base: '/image-resizer/', // only affects build
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      // ensure file-saver is bundled
-      external: [],
-    },
   },
   optimizeDeps: {
-    include: ['file-saver', 'utif'] // force Vite to pre-bundle these
-  }
+    include: ['file-saver', 'utif'],
+  },
 })
